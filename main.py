a6012f3cc7700631652f8923750d94bd003a78de
Leonardo Ikeda <80907803+ikedss@users.noreply.github.com>
2022-10-20 19:21:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,14 +13,18 @@ def adiciona_site(site, lsentencas):
     for doc in soap.find_all('p'):
         documento = nlp(doc.text)
         for sentenca in documento.sents:
-            lsentencas.append(sentenca.text)
+            lsentencas.append(sentenca)
     return lsentencas
 
-sentencas = []
-sentencas1 = adiciona_site("https://en.wikipedia.org/wiki/Natural_language_processing", sentencas)
-sentencas2 = adiciona_site("https://www.ibm.com/cloud/learn/natural-language-processing", sentencas)
-sentencas3 = adiciona_site("https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html", sentencas)
-sentencas4 = adiciona_site("https://builtin.com/data-science/high-level-guide-natural-language-processing-techniques", sentencas)
-sentencas5 = adiciona_site("https://deepsense.ai/a-business-guide-to-natural-language-processing-nlp/", sentencas)
+lsentencas1 = []
+lsentencas2 = []
+lsentencas3 = []
+lsentencas4 = []
+lsentencas5 = []
+sentencas1 = adiciona_site("https://en.wikipedia.org/wiki/Natural_language_processing", lsentencas1)
+sentencas2 = adiciona_site("https://www.ibm.com/cloud/learn/natural-language-processing", lsentencas2)
+sentencas3 = adiciona_site("https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html", lsentencas3)
+sentencas4 = adiciona_site("https://builtin.com/data-science/high-level-guide-natural-language-processing-techniques", lsentencas4)
+sentencas5 = adiciona_site("https://deepsense.ai/a-business-guide-to-natural-language-processing-nlp/", lsentencas5)
 
-print(sentencas)
+print(lsentencas1, lsentencas2, lsentencas3, lsentencas4, lsentencas5)
