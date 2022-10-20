97e2327b0e5a3bbd9f6a65fc7912b978d8e82358
Leonardo Ikeda <80907803+ikedss@users.noreply.github.com>
2022-10-20 18:06:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Aluno: Leonardo Ikeda
+# Aluno: Leonardo Ikeda
 
 from bs4 import BeautifulSoup
 import requests
@@ -6,37 +6,21 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
-html1 = requests.get("https://en.wikipedia.org/wiki/Natural_language_processing")
-html2 = requests.get("https://www.ibm.com/cloud/learn/natural-language-processing")
-html3 = requests.get("https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html")
-html4 = requests.get("https://builtin.com/data-science/high-level-guide-natural-language-processing-techniques")
-html5 = requests.get("https://deepsense.ai/a-business-guide-to-natural-language-processing-nlp/")
-soup1 = BeautifulSoup(html1.text, "html.parser")
-soup2 = BeautifulSoup(html2.text, "html.parser")
-soup3 = BeautifulSoup(html3.text, "html.parser")
-soup4 = BeautifulSoup(html4.text, "html.parser")
-soup5 = BeautifulSoup(html5.text, "html.parser")
-#print(soup1.get_text(), soup2.get_text(), soup3.get_text(), soup4.get_text(), soup5.get_text())
+def adiciona_site(site, lsentencas):
+    html = requests.get(site)
+    soap = BeautifulSoup(html.content, 'html.parser')
 
-doc1 = nlp(soup1.text)
-doc2 = nlp(soup2.text)
-doc3 = nlp(soup3.text)
-doc4 = nlp(soup4.text)
-doc5 = nlp(soup5.text)
-
-sentencas = []
-
-def get_sentencas(lsentencas, doc):
-    contador = 0
-    for sentenca in doc.sents:
-      lsentencas.append(sentenca.text)
-      contador += 1
+    for doc in soap.find_all('p'):
+        documento = nlp(doc.text)
+        for sentenca in documento.sents:
+            lsentencas.append(sentenca.text)
     return lsentencas
 
-sentencas1 = get_sentencas(sentencas, doc1)
-sentencas2 = get_sentencas(sentencas, doc2)
-sentencas3 = get_sentencas(sentencas, doc3)
-sentencas4 = get_sentencas(sentencas, doc4)
-sentencas5 = get_sentencas(sentencas, doc5)
+sentencas = []
+sentencas1 = adiciona_site("https://en.wikipedia.org/wiki/Natural_language_processing", sentencas)
+sentencas2 = adiciona_site("https://www.ibm.com/cloud/learn/natural-language-processing", sentencas)
+sentencas3 = adiciona_site("https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html", sentencas)
+sentencas4 = adiciona_site("https://builtin.com/data-science/high-level-guide-natural-language-processing-techniques", sentencas)
+sentencas5 = adiciona_site("https://deepsense.ai/a-business-guide-to-natural-language-processing-nlp/", sentencas)
 
 print(sentencas)
